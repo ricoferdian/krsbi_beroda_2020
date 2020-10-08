6bb00911652499f452b6c851743ace60d4319c0a
Henrico Aldy Ferdian <henricoaldyferdian@hotmail.com>
2020-10-08 13:22:19 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -42,8 +42,15 @@ PORT = 28097
 # networkserial.connect((HOST, PORT))
 
 def detect(save_img=False):
+    #ROBOT 2
+    # out, source, weights, view_img, save_txt, imgsz = \
+    #     'inference/output', '0', 'D:\\Libraries\\Project\\Python\\yolov5\\runs\\exp8\\weights\\best.pt', None, None, 640
+    # webcam = source == '0' or source == '1' or source.startswith('rtsp') or source.startswith(
+    #     'http') or source.endswith('.txt')
+
+    #ROBOT 1
     out, source, weights, view_img, save_txt, imgsz = \
-        'inference/output', '0', 'D:\\Libraries\\Project\\Python\\yolov5\\runs\\exp8\\weights\\best.pt', None, None, 640
+        'inference/output', '1', 'D:\\Libraries\\Project\\Python\\yolov5\\runs\\exp8\\weights\\best.pt', None, None, 640
     webcam = source == '0' or source == '1' or source.startswith('rtsp') or source.startswith(
         'http') or source.endswith('.txt')
 
