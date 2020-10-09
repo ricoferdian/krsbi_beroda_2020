96178ffb951d2c9f9e08ad170f8bdb76f59416bc
Henrico Aldy Ferdian <henricoaldyferdian@hotmail.com>
2020-10-10 03:35:16 +0800

    LAPTOP DEK JUN

--- getcoord.py
+++ getcoord.py
@@ -1,12 +1,12 @@
 from helper_new import *
 
 # LAPTOP UCUP
-cameraCenterX = 343
-cameraCenterY = 238
+# cameraCenterX = 343
+# cameraCenterY = 238
 
 # LAPTOP DEK JUN
-# cameraCenterX = 295
-# cameraCenterY = 248
+cameraCenterX = 295
+cameraCenterY = 248
 
 def getcoordinate(arr_objects):
     for index, object in enumerate(arr_objects):
